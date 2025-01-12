db92c6c958d6637cf69d381c4220142e13502840
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2025-01-12 15:31:09 +0100

    Actualizado el cambio de imagen del logo según el modo de tema (claro/oscuro) en el archivo HTML y JavaScript.

--- js/theme-mode.js
+++ js/theme-mode.js
@@ -2,13 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const toogleButton = document.getElementById('darkmode-toggle');
     toogleButton.addEventListener('change', () => {
         document.body.classList.toggle('dark-mode');
+        const logoImg = document.querySelector('.logo a img');
 
         // Cambiar iconos y colores según el tema
         if (document.body.classList.contains('dark-mode')) {
+            logoImg.setAttribute('src', '/images/1-dark.png');
             var primaryColor = getComputedStyle(document.documentElement).getPropertyValue("--primary-color");
             var secondaryColor = getComputedStyle(document.documentElement).getPropertyValue("--secondary-color");
             darkOn(primaryColor, secondaryColor); // Cambia a modo oscuro
         } else {
+            logoImg.setAttribute('src', '/images/2-light.png');
             var primaryColor = getComputedStyle(document.documentElement).getPropertyValue("--primary-color");
             var secondaryColor = getComputedStyle(document.documentElement).getPropertyValue("--secondary-color");
             lightOn(primaryColor, secondaryColor); // Cambia a modo claro
