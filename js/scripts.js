80e0a1bf3b5de9eeafa82656c3a40a8e9d9d5667
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2025-09-11 23:39:25 +0200

    Añadido formulario de contacto con validación y funcionalidad de envío por correo electrónico.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,56 @@ let menuMobile = document.getElementById('menu-mobile');
 let overlayMenu = document.getElementById('overlay-menu');
 
 
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var telefono = document.getElementById('telefono').value;
+    var mensaje = document.getElementById('mensaje').value;
+
+    if (!validarEmail(email)) {
+        alert('Email inválido');
+        e.preventDefault();
+        return;
+    }
+    if (!validarTelefono(telefono)) {
+        alert('Teléfono inválido');
+        e.preventDefault();
+        return;
+    }
+
+    var body =
+        `--------------------------%0A` +
+        `📝 Solicitud de Contacto%0A` +
+        `--------------------------%0A%0A` +
+        `👤 Nombre Completo: ${nombre}%0A` +
+        `✉️ Email: ${email}%0A` +
+        `📞 Teléfono: ${telefono}%0A%0A` +
+        `💬 Mensaje:%0A${mensaje}%0A%0A` +
+        `--------------------------%0A` +
+        `Recibido automáticamente desde la web.%0A%0A`;
+
+    // Uso en mailto:
+    var mailtoLink = `mailto:contact@virosms.com?subject=Nuevo Contacto Web de ${nombre}&body=${body}`;
+    window.location.href = mailtoLink;
+
+});
+
+function validarEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+}
+
+function validarTelefono(telefono) {
+    const regex = /^[0-9 +-]{7,16}$/; // solo números, espacios, + y -
+    return telefono.trim() === '' || regex.test(telefono); // opcional pero válido
+}
+
+
+
+
+
 btnMenu.addEventListener('click', () => {
     menuMobile.classList.add('open-menu');
 });
